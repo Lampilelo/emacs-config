ebc0a8af1333d038549535d5dd588c7f63731e74
Lampilelo <cubex7@gmail.com>
2017-06-07 16:24:34 +0200

    Set default browser for opening links to Vivaldi

--- init.el
+++ init.el
@@ -232,9 +232,14 @@
 ;; MAGIT
 (global-set-key (kbd "C-x g") 'magit-status)
 
+;; Set default browser for opening links
+(setq browse-url-browser-function 'browse-url-generic
+      browse-url-generic-program "vivaldi")
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:background nil)))))
+
