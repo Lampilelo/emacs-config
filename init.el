28d4e1dda351450aa018fb0386ef905f7cda13ee
Lampilelo <cubex7@gmail.com>
2017-07-28 11:36:23 +0200

    Moved ERC config to separate file.

--- init.el
+++ init.el
@@ -266,57 +266,5 @@
             (if (eq window-system 'x)
                 (font-lock-mode 1))))
 
-
-;; ==================== ERC ====================
-;; ident
-(setq
- erc-nick "Oxon"
- erc-server "irc.rizon.net"
- erc-prompt-for-password nil
- erc-prompt-for-nickserv-password nil)
-;;(erc :server "irc.rizon.net" :port 6667 :nick "Oxon")
-(add-hook 'erc-mode-hook 'erc-nickserv-mode)
-(load "~/.erc/passwords")
-(setq erc-nickserv-passwords
-      `((Rizon (("Oxon" . ,rizon-oxon-pass)))))
-;; logs
-(require 'erc-log)
-(erc-log-enable)
-(setq erc-log-channels-directory "~/.erc/logs/")
-(setq erc-save-buffer-on-part t)
-;; load modules
-(require 'erc-services)
-(setq erc-services-enable t)
-;; (require 'erc-notifications)
-;; 'erc-notifications-enable t
-
-;; auto-join channels
-(erc-autojoin-mode 0)
-(setq erc-autojoin-channels-alist
-      '((".*rizon.*" "#krasnale" "#emurh")))
-(add-hook 'erc-server-NOTICE-functions 'my-post-vhost-autojoin)
-(defun my-post-vhost-autojoin (proc parsed)
-  "Autojoin when NickServ tells us to."
-  (with-current-buffer (process-buffer proc)
-    (when (string-match ".*Password accepted.*"
-                             (erc-response.contents parsed))
-      (erc-autojoin-channels erc-session-server (erc-current-nick))
-      nil)))
-;; misc options
-;; Kill buffers for channels after /part
-(setq erc-kill-buffer-on-part t)
-;; Kill buffers for private queries after quitting the server
-(setq erc-kill-queries-on-quit t)
-;; Kill buffers for server messages after quitting the server
-(setq erc-kill-server-buffer-on-quit t)
- ;; Interpret mIRC-style color commands in IRC chats
-(setq erc-interpret-mirc-color t)
-
-;; =============================================
-
-;; PYTHON
-(setq flycheck-python-pycompile-executable "python3")
-
-;; WHICH KEY
-(require 'which-key)
-(add-hook 'after-init-hook 'which-key-mode)
+;; ERC
+(load "~/.emacs.d/erc-init.el")
