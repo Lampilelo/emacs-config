43e506bd11009dc61ddcbdc4ded6a04f8498eb15
Lampilelo <cubex7@gmail.com>
2017-07-28 14:53:08 +0200

    Check for build directory added.

--- get-rtags.sh
+++ get-rtags.sh
@@ -4,5 +4,6 @@ EMACS_DIR="$HOME/.emacs.d"
 
 cd $EMACS_DIR && git clone --recursive https://github.com/Andersbakken/rtags.git
 
+if [ -x $EMACS_DIR/rtags/build ]; then rm -r $EMACS_DIR/rtags/build; fi
 mkdir $EMACS_DIR/rtags/build
 cd $EMACS_DIR/rtags/build && cmake .. && make
