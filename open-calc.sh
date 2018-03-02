f04876dfd936eff18e0b2822ffcabc4fe3477b5e
Lampilelo <cubex7@gmail.com>
2018-03-02 22:38:53 +0100

    Added script for starting calc in new, no-init instance of emacs.

---
+++ open-calc.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+emacs --no-init-file \
+      --no-window-system \
+      --eval '(progn (menu-bar-mode -1)
+      	             (calc)
+	             (let* ((buffer (get-buffer "*scratch")))
+	               (delete-windows-on buffer)
+		       (kill-buffer buffer))
+		     (define-key calc-mode-map (kbd "q") nil))'
