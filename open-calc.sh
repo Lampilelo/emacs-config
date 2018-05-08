cf0ae4d95a7fafc10cf5cd9b9d222561b187fb71
Lampilelo <cubex7@gmail.com>
2018-05-08 11:27:28 +0200

    Bound 'q' to save-buffers-kill-terminal.

--- open-calc.sh
+++ open-calc.sh
@@ -2,9 +2,9 @@
 
 emacs --no-init-file \
       --no-window-system \
-      --eval '(progn (menu-bar-mode -1)
+      --eval "(progn (menu-bar-mode -1)
       	             (calc)
-	             (let* ((buffer (get-buffer "*scratch")))
+	             (let* ((buffer (get-buffer \"*scratch\")))
 	               (delete-windows-on buffer)
 		       (kill-buffer buffer))
-		     (define-key calc-mode-map (kbd "q") nil))'
+		     (define-key calc-mode-map (kbd \"q\") 'save-buffers-kill-terminal))"
