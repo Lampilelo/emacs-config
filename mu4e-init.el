cb62db13134fa58a90078e69739c59cccb5383c5
Lampilelo <cubex7@gmail.com>
2018-06-28 15:10:40 +0200

    mu4e-get-mail-command no longer is asynchronous

--- mu4e-init.el
+++ mu4e-init.el
@@ -10,7 +10,7 @@
 (setq mu4e-compose-context-policy 'pick-first)
 
 ;; Custom command for updating email.
-(setq mu4e-get-mail-command "systemctl --user start mbsync@$USER.service &")
+(setq mu4e-get-mail-command "systemctl --user start mbsync@$USER.service")
 
 (setq mu4e-change-filenames-when-moving t)
 (setq mu4e-maildir "~/Mail")
