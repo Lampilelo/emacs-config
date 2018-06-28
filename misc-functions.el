27b12f82414405d30fde2b867d48080b88ed712c
Lampilelo <cubex7@gmail.com>
2018-06-28 15:10:10 +0200

    Added two examples.

--- misc-functions.el
+++ misc-functions.el
@@ -21,3 +21,24 @@
       (forward-line))
     (org-table-align)
 ))
+
+
+(defun M-x-other-window ()
+      "Call M-x in the other window."
+      (interactive)
+      (save-selected-window
+        (other-window 1)
+        (execute-extended-command nil)))
+
+
+;; loop example
+(defun temp/find-root () (interactive)
+       (message "%s"
+		(loop for (key . value) in my/c++-build-systems-alist
+		      for var = (vc-find-root buffer-file-name key)
+		      until (not (eq var nil))
+		      finally return var)))
+
+
+;; IN-PROGRESS
+
