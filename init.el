ecf3701ec4a1c09c5f71d48c16b89e5d66ad599f
Lampilelo <cubex7@gmail.com>
2018-07-31 00:00:57 +0200

    Added alternative bindings to backspace.

--- init.el
+++ init.el
@@ -229,6 +229,8 @@ With a prefix argument \\[universal-argument], just call generic ‘helm-info’
 (global-set-key (kbd "M-o") #'other-window)
 (global-set-key (kbd "M-i") #'imenu)
 (global-set-key (kbd "C-x k") #'kill-this-buffer)
+(global-set-key (kbd "C-z") (kbd "DEL"))   ; backward-delete-char
+(global-set-key (kbd "M-z") (kbd "M-DEL")) ; backward-kill-word
 
 ;; Monday as first day of the week
 (setq calendar-week-start-day 1)
