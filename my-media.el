d03a7a99d3696b923e3bfff97a7d92df4bed7eb1
jakub-w <jakub-w@riseup.net>
2020-11-10 11:36:10 +0100

    New stuff in my-media.el
    - Added support for opus files
    - Changed some errors to user-errors
    - Removed placeholder buffer in id3-exec
    - Added media-split-flac function because media-split-album has issues

--- my-media.el
+++ my-media.el
@@ -1,24 +1,29 @@
 ;; -*- lexical-binding: t -*-
 
+(require 'seq)
+(require 'cl-lib)
+
 (defvar media--vorbis-exts '("ogg"))
 (defvar media--flac-exts '("flac"))
 (defvar media--id3-exts '("mp3"))
+(defvar media--opus-exts '("opus"))
 (defvar media--tags-dispatch
   `((,(regexp-opt media--vorbis-exts) :set media--set-vorbis-tags)
     (,(regexp-opt media--id3-exts) :set media--set-id3-tags)
-    (,(regexp-opt media--flac-exts) :set media--set-flac-tags))
+    (,(regexp-opt media--flac-exts) :set media--set-flac-tags)
+    (,(regexp-opt media--opus-exts) :set media--set-opus-tags))
   "An alist. The first value is a key in the form of a regex to match.
 The cdr of every entry is a plist of setters (:set) and getters (:get).")
 
 (defun media--check-extension (ext-list filename)
   (unless (member (file-name-extension filename) ext-list)
-    (error "Wrong file type" filename)))
+    (user-error "Wrong file type" filename)))
 
 (defun media--set-vorbis-tags (artist album year title track-num filename)
   (setq filename (shell-quote-argument filename))
   (media--check-extension media--vorbis-exts filename)
   (unless (executable-find "vorbiscomment")
-    (error "Could not find vorbiscomment"))
+    (user-error "Could not find vorbiscomment"))
   (let ((tags (mapcar (lambda (tag)
 			(string-match "\\([^=]+?\\)=\\(.*\\)" tag)
 			(cons (upcase (match-string-no-properties 1 tag))
@@ -43,7 +48,7 @@ The cdr of every entry is a plist of setters (:set) and getters (:get).")
 (defun media--set-flac-tags (artist album year title track-num filename)
   (media--check-extension media--flac-exts filename)
   (unless (executable-find "metaflac")
-    (error "Could not find metaflac"))
+    (user-error "Could not find metaflac"))
   (let (tag-args)
     (when artist
       (push (format "--set-tag=ARTIST=%s" artist) tag-args)
@@ -64,16 +69,35 @@ The cdr of every entry is a plist of setters (:set) and getters (:get).")
 
 (defun media--set-id3-tags (artist album year title track-num filename)
   (media--check-extension media--id3-exts filename)
-  (let ((id3-exec (or (executable-find "id3tag")
-		      (executable-find "id3v2")
-		      (error "Could not find neither id3tag nor id3v2"))))
-    (call-process id3-exec nil "foobarlol" nil
+  (let ((id3-exec
+	 (or (executable-find "id3tag")
+	     (executable-find "id3v2")
+	     (user-error "Could not find neither id3tag nor id3v2"))))
+    (call-process id3-exec nil nil nil
 		  "--artist" artist
 		  "--album" album
 		  "--year" (format "%s" year)
 		  "--song" title
 		  "--track" (format "%s" track-num)
 		  filename)))
+
+(defun media--set-opus-tags (artist album year title track-num filename)
+  (media--check-extension media--opus-exts filename)
+  (let ((opus-exec (or (executable-find "ffmpeg")
+		       (user-error "Could not find ffmpeg")))
+	(temp-file (format "%s.opus" (make-temp-name "/tmp/music-file-"))))
+    (when (= 0 (call-process opus-exec nil "*my-test-ffmpeg*" nil
+			     "-i" filename
+			     "-metadata" (format "artist=%s" artist)
+			     "-metadata" (format "album=%s" album)
+			     "-metadata" (format "date=%s" year)
+			     "-metadata" (format "title=%s" title)
+			     "-metadata" (format "tracknumber=%s" track-num)
+			     "-codec" "copy"
+			     temp-file))
+      (copy-file temp-file filename t)
+      (delete-file temp-file)
+      t)))
 
 (defun media-set-tags (artist album year title track-num filename)
   "Set id3 tags, vorbis comments or flac metadata of a music file."
@@ -142,3 +166,37 @@ So every entry is: (<number> \"<name>\" \"<start-time>\" \"<end-time>\")"
 	 input-file from to outfile
 	 (lambda (outfile)
 	   (media-set-tags artist album year title track-num outfile)))))))
+
+(defun media-split-flac (cue-file flac-file)
+  (cl-assert (and (executable-find "shnsplit")
+		  (executable-find "cuetag.shs")))
+  (with-temp-buffer
+    (insert-file-contents cue-file)
+    (let* ((year (progn
+		   (goto-char (point-min))
+		   (search-forward-regexp "DATE \\([[:digit:]]\\{4\\}\\)")
+		   (match-string 1)))
+	   (album
+	    (progn (goto-char (point-min))
+		   (search-forward-regexp "TITLE \"\\(.+\\)\"$")
+		   (match-string 1)))
+	   (outdir (format "%s(%s) %s/"
+			  (file-name-directory flac-file)
+			  year
+			  album)))
+      (make-directory outdir t)
+      (erase-buffer)
+      (unless (= 0 (call-process "shnsplit" nil t nil
+				 "-f" cue-file
+				 "-t" "%n. %t"
+				 "-d" outdir
+				 "-o" "flac"
+				 flac-file))
+	(error "Couldn't split the flac file: %s" (buffer-string)))
+      (erase-buffer)
+      (unless (= 0 (apply #'call-process "cuetag.sh" nil t nil
+			  cue-file
+			  (directory-files outdir t "^[^.].*")))
+	(error ("Couldn't apply the tags to resulting flac files: %s"
+		(buffer-string)))))))
+
